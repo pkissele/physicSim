033bfd989b4bc219a18cfd2b3b108a073983132b
pkissele <peter.kisselev@gmail.com>
2026-05-18 22:44:58 -0400

    Small

--- scripts/img2gif.py
+++ scripts/img2gif.py
@@ -15,6 +15,7 @@ if len(argv) > 3:
     durationLen = float(argv[3])
 
 for j in range(numframes):
+    print(j)
     num = f'{j:05d}'
     img = Image.open(f'{folderName}/{prefix}{num}.png')
     ani.append(img.copy())
