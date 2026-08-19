30d2723ee567eaba215b4f74162594c005ab433d
pkissele <peter.kisselev@gmail.com>
2026-08-19 02:24:50 -0400

    Cap neighbor memory, sweep time, parallelize fluid compute

--- scripts/sweep.sh
+++ scripts/sweep.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -e
- 
+
 CONSTS_FILE="include/consts.h"
 MASSES=(1 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80)
-# MASSES=(5)
 SEEDS=(1 2 3 4 5)
-# SEEDS=(1 2 3)
 STEPS=$((1500 + 100))
-OUT=runs
-mkdir -p "$OUT"
-
-git rev-parse HEAD > "$OUT/commit.txt"
 
 for mass in "${MASSES[@]}"; do
     for seed in "${SEEDS[@]}"; do
